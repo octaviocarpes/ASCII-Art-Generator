b5211a80dc01f961453c4d181443e2575a558b7d
13202108 <13202108@facin.portoalegre.pucrsnet.br>
2018-09-14 20:00:03 -0300

    Added loader

---
+++ loader/main.c
@@ -0,0 +1,51 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>		// Para usar strings
+
+// SOIL é a biblioteca para leitura das imagens
+#include "SOIL.h"
+
+// Um pixel RGB
+typedef struct {
+    unsigned char r, g, b;
+} RGB;
+
+// Uma imagem em RGB
+typedef struct {
+    int width, height;
+    RGB* img;
+} Img;
+
+// Protótipos
+void load(char* name, Img* pic);
+
+// Carrega uma imagem para a struct Img
+void load(char* name, Img* pic)
+{
+    int chan;
+    pic->img = (unsigned char*) SOIL_load_image(name, &pic->width, &pic->height, &chan, SOIL_LOAD_RGB);
+    if(!pic->img)
+    {
+        printf( "SOIL loading error: '%s'\n", SOIL_last_result() );
+        exit(1);
+    }
+    printf("Load: %d x %d x %d\n", pic->width, pic->height, chan);
+}
+
+int main(int argc, char** argv)
+{
+    Img pic;
+    if(argc < 1) {
+        printf("loader [img]\n");
+        exit(1);
+    }
+    load(argv[1], &pic);
+
+    printf("Primeiros 10 pixels da imagem:\n");
+    for(int i=0; i<10; i++) {
+        printf("[%02X %02X %02X] ", pic.img[i].r, pic.img[i].g, pic.img[i].b);
+    }
+    printf("\n");
+
+    free(pic.img);
+}
